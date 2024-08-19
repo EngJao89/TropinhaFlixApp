ba31d992fecf106fa1a59dbee5311709e9f33b37
EngJao89 <eng_joao@outlook.com>
2024-08-18 23:01:42 -0300

    feat(tfa10-movie-header): implementa header da tela de filmes favoritos

--- src/components/MovieHeader/index.tsx
+++ src/components/MovieHeader/index.tsx
@@ -1,13 +1,13 @@
 import {Text, View} from 'react-native';
-import {FilmSlate} from 'phosphor-react-native';
+import {FilmReel} from 'phosphor-react-native';
 
 import {theme} from '../../styles/theme';
 import {styles} from './styles';
 
-export function Header() {
+export function MovieHeader() {
   return (
     <View style={styles.container}>
-      <FilmSlate color={theme.colors.white} size={24} />
+      <FilmReel color={theme.colors.white} size={24} />
       <Text style={styles.title}>Tropinha Flix</Text>
     </View>
   );
