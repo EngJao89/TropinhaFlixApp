b2a3890bda75b3ceea1c8d13153769bac9b0675f
EngJao89 <eng_joao@outlook.com>
2024-08-19 23:20:56 -0300

    feat(tfa11-search-item): implementa componente de busca de filmes

--- src/components/SearchItem/index.tsx
+++ src/components/SearchItem/index.tsx
@@ -1,13 +1,54 @@
-import {Image, View} from 'react-native';
+import {Alert, Image, Text, TouchableOpacity, View} from 'react-native';
+import {Star} from 'phosphor-react-native';
+
 import {styles} from './styles';
 
-export function SearchItem() {
+interface SearchItemProps {
+  data: {
+    poster_path?: string;
+    release_date?: string;
+    title?: string;
+    vote_average?: number;
+  };
+  navigatePage: (data: any) => void;
+}
+
+export function SearchItem({data, navigatePage}: SearchItemProps) {
+  const detailMovie = () => {
+    if (data.release_date === '') {
+      Alert.alert('Filme ainda sem data');
+      return;
+    }
+    navigatePage(data);
+  };
+
   return (
-    <View style={styles.container}>
-      <Image
-        resizeMethod="resize"
-        source={require('../../assets/image 1.png')}
-      />
-    </View>
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={detailMovie}
+      style={styles.container}>
+      {data?.poster_path ? (
+        <Image
+          resizeMethod="resize"
+          source={{
+            uri: `https://image.tmdb.org/t/p/original/${data?.poster_path}`,
+          }}
+          style={styles.banner}
+        />
+      ) : (
+        <Image
+          resizeMethod="resize"
+          source={require('../../assets/image 1.png')}
+          style={styles.banner}
+        />
+      )}
+
+      <Text style={styles.title}>{data?.title}</Text>
+
+      <View style={styles.rateContainer}>
+        <Star size={12} color="#E7A74e" />
+        <Text style={styles.rate}>{data?.vote_average}/10</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
---
+++ src/screens/Search/styles.ts
@@ -0,0 +1,9 @@
+import {theme} from '../../styles/theme';
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.gray_700,
+  },
+});
