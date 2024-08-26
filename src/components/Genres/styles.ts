c34532ab8192d9c554582a47a6cc8f0b3ea3bdb1
EngJao89 <eng_joao@outlook.com>
2024-08-25 21:34:30 -0300

    refactor(tfa22-changes-details): melhora rotas de componentes de detalhes

--- src/components/Genres/styles.ts
+++ src/components/Genres/styles.ts
@@ -3,11 +3,13 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
+    marginTop: 8,
     marginRight: 8,
     backgroundColor: theme.colors.gray_300,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 5,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
     borderRadius: 8,
   },
 });
